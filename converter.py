11a1b8a07a6ebd4a7475921be7430b8efe50c9cc
Caden Pun <70039430+Cpun077@users.noreply.github.com>
2024-02-28 19:29:04 -0800

    mp3s

--- converter.py
+++ converter.py
@@ -1,12 +1,27 @@
-from pytube import YouTube, Playlist
+from pytube import Playlist
+from moviepy.editor import AudioFileClip
 import os
+import re
 
 def dlpl(pl, dir):
-    p = Playlist(pl)
-    for video in p.videos:
-        video.streams.first().download(dir)
-        
+    try:
+        p = Playlist(pl)
+        for video in p.videos:
+            audios = video.streams.filter(only_audio=True)
+            best = max(audios, key=lambda x: int(x.abr[:-4])) # remove the kbps suffix
+            best.download(dir)
+        for a in os.listdir(dir):
+            if not re.search("mp3", a):
+                path = os.path.join(dir, a)
+                audio = AudioFileClip(path)
+                newpath = os.path.splitext(path)[0] + '.mp3'
+                audio.write_audiofile(newpath)
+                os.remove(path)
+        print("completed")
+    except Exception as e:
+        print("error: ", e)
+
 dlpl(
     "https://music.youtube.com/playlist?list=PL4ckrlA4uj4tr28D-Dpn5QrrS0wxd2blM&si=Z_8eTkBKgvTQm-U3",
-    os.path.join(os.path.expanduser('~'), 'Downloads')
+    os.path.join(os.path.expanduser('~'), 'Downloads/Sounds')
     )
