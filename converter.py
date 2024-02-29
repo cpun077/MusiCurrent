98cb0a22e538bcabb805203c62f2c90fbac87ec4
Caden Pun <70039430+Cpun077@users.noreply.github.com>
2024-02-29 12:20:15 -0800

    remove format conversion

--- converter.py
+++ converter.py
@@ -1,7 +1,5 @@
 from pytube import Playlist
-from moviepy.editor import AudioFileClip
 import os
-import re
 
 def dlpl(pl, dir):
     try:
@@ -10,16 +8,22 @@ def dlpl(pl, dir):
             audios = video.streams.filter(only_audio=True)
             best = max(audios, key=lambda x: int(x.abr[:-4])) # remove the kbps suffix
             best.download(dir)
-        for a in os.listdir(dir):
-            if not re.search("mp3", a):
-                path = os.path.join(dir, a)
-                audio = AudioFileClip(path)
-                newpath = os.path.splitext(path)[0] + '.mp3'
-                audio.write_audiofile(newpath)
-                os.remove(path)
         print("completed")
     except Exception as e:
         print("error: ", e)
+
+# def dlpl(pl, dir):
+#     try:
+#         p = Playlist(pl)
+#         config = {
+#             'format': 'bestaudio',
+#             'outtmpl': f'{dir}/%(title)s.%(ext)s',
+#         }
+#         with YoutubeDL(config) as dl:
+#             dl.download(p.video_urls)
+#         print("completed")
+#     except Exception as e:
+#         print("error: ", e)
 
 dlpl(
     "https://music.youtube.com/playlist?list=PL4ckrlA4uj4tr28D-Dpn5QrrS0wxd2blM&si=Z_8eTkBKgvTQm-U3",
