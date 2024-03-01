73b69fa5670eb1c58dca8d6070a8b54a2890576b
Caden Pun <70039430+Cpun077@users.noreply.github.com>
2024-02-29 16:16:39 -0800

    test connection to swift

--- converter.py
+++ converter.py
@@ -12,6 +12,9 @@ def dlpl(pl, dir):
     except Exception as e:
         print("error: ", e)
 
+def testConnect(msg):
+    print(msg)
+
 # def dlpl(pl, dir):
 #     try:
 #         p = Playlist(pl)
@@ -25,7 +28,7 @@ def dlpl(pl, dir):
 #     except Exception as e:
 #         print("error: ", e)
 
-dlpl(
-    "https://music.youtube.com/playlist?list=PL4ckrlA4uj4tr28D-Dpn5QrrS0wxd2blM&si=Z_8eTkBKgvTQm-U3",
-    os.path.join(os.path.expanduser('~'), 'Downloads/Sounds')
-    )
+# dlpl(
+#     "https://music.youtube.com/playlist?list=PL4ckrlA4uj4tr28D-Dpn5QrrS0wxd2blM&si=Z_8eTkBKgvTQm-U3",
+#     os.path.join(os.path.expanduser('~'), 'Downloads/Sounds')
+#     )
