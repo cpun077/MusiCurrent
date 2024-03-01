e65e6f728d3c9f0036270f7b2bb85ef07e9ef9c5
Cpun077 <cadenpun@gmail.com>
2024-02-29 16:14:20 -0800

    create xcode project

---
+++ playlist-localizer/playlist-localizer/playlist_localizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  playlist_localizerApp.swift
+//  playlist-localizer
+//
+//  Created by Caden Pun on 2/29/24.
+//
+
+import SwiftUI
+
+@main
+struct playlist_localizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
