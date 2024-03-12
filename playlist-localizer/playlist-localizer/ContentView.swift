fc665b6a59170802bed8fc9efd4b0bc4ad3c617f
Cpun077 <cadenpun@gmail.com>
2024-03-11 23:41:55 -0700

    macos exclusive, stacknavigation ui

--- playlist-localizer/playlist-localizer/ContentView.swift
+++ playlist-localizer/playlist-localizer/ContentView.swift
@@ -11,20 +11,53 @@ class FormViewModel: ObservableObject {
     @Published var link = ""
 }
 
+enum currentView {
+    case root
+    case subview
+}
+
 struct ContentView: View {
     @State var viewModel = FormViewModel()
+    @State private var currentView : currentView = .root
     
     var body: some View {
-        GeometryReader { geometry in
-            NavigationView {
-                VStack {
-                    Form {
-                        Section(footer: Text("Your link must be from a supported platform.")) {
-                            TextField("Link", text: $viewModel.link)
-                        }
+        VStack {
+            switch currentView {
+            case .root:
+                root
+            case .subview:
+                subview
+            }
+        }
+        .toolbar {
+            ToolbarItem(placement: .navigation) {
+                Button(action: {
+                    withAnimation(.easeOut(duration: 0.3)) {
+                        currentView = .root
                     }
-                    Button(action: {
-                        // downloadsurl = "/Users/cadenpun/Downloads"
+                }, label: {
+                    Label("back", systemImage: "chevron.left")
+                })
+                .disabled(currentView == .root)
+                .help("Go Back")
+            }
+        }
+    }
+    
+    var root: some View {
+        GeometryReader { geometry in
+            VStack {
+                Form {
+                    Section(footer: Text("Your link must be from a supported platform.")) {
+                        TextField("Link", text: $viewModel.link)
+                    }
+                }
+                Button(action: {
+                    withAnimation(.easeOut(duration: 0.3)) {
+                        currentView = .subview
+                    }
+                    
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
                         if let downloads = FileManager.default.urls(for: .downloadsDirectory, in: .userDomainMask).first {
                             let sfolder = downloads.appendingPathComponent("Sounds")
                             if !FileManager.default.fileExists(atPath: sfolder.path) {
@@ -39,20 +72,25 @@ struct ContentView: View {
                         } else {
                             print("Could not access Downloads folder")
                         }
-                    }) {
-                        Text("Submit")
-                            .frame(width: geometry.size.width / 2, height: geometry.size.height / 15)
-                            .foregroundColor(.white)
-                            .background(Color.blue)
-                            .cornerRadius(15)
                     }
-                    .padding()
+                }) {
+                    Text("Submit")
+                        .frame(width: geometry.size.width / 2, height: geometry.size.height / 15)
+                        .foregroundColor(.white)
+                        .background(Color.blue)
+                        .cornerRadius(15)
                 }
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-                .navigationTitle("Paste Playlist")
-                .padding()
             }
         }
+    }
+    
+    var subview: some View {
+        Text("Downloading Songs").frame(maxWidth: .infinity, maxHeight: .infinity)
+            .onTapGesture {
+                withAnimation(.easeOut(duration: 0.3)) {
+                    currentView = .root
+                }
+            }
     }
 }
 
--- playlist-localizer/playlist-localizer/playlist_localizerApp.swift
+++ playlist-localizer/playlist-localizer/playlist_localizerApp.swift
@@ -6,12 +6,25 @@
 //
 
 import SwiftUI
+import AppKit
 
 @main
 struct playlist_localizerApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .frame(minWidth: 700, minHeight: 500)
+        }
+    }
+}
+
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        if let window = NSApplication.shared.windows.first {
+            window.center()
         }
     }
 }
