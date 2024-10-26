93f6fb55096f2119774f1d03c70f8a2e7f220d64
Cpun077 <cadenpun@gmail.com>
2024-10-25 23:47:34 -0700

    change to pytubefix

--- MusiCurrent/MusiCurrent/converter.py
+++ MusiCurrent/MusiCurrent/converter.py
@@ -1,63 +1,12 @@
-from pytube import Playlist, YouTube
+from pytubefix import Playlist, YouTube
 from moviepy.editor import AudioFileClip
 import os, subprocess, platform
-# temporary fix to get_throttling_function_name error
-from pytube import cipher
-import re
-
-def get_throttling_function_name(js: str) -> str:
-    """Extract the name of the function that computes the throttling parameter.
-
-    :param str js:
-        The contents of the base.js asset file.
-    :rtype: str
-    :returns:
-        The name of the function used to compute the throttling parameter.
-    """
-    function_patterns = [
-        # https://github.com/ytdl-org/youtube-dl/issues/29326#issuecomment-865985377
-        # https://github.com/yt-dlp/yt-dlp/commit/48416bc4a8f1d5ff07d5977659cb8ece7640dcd8
-        # var Bpa = [iha];
-        # ...
-        # a.C && (b = a.get("n")) && (b = Bpa[0](b), a.set("n", b),
-        # Bpa.length || iha("")) }};
-        # In the above case, `iha` is the relevant function name
-        r'a\.[a-zA-Z]\s*&&\s*\([a-z]\s*=\s*a\.get\("n"\)\)\s*&&\s*'
-        r'\([a-z]\s*=\s*([a-zA-Z0-9$]+)(\[\d+\])?\([a-z]\)',
-        r'\([a-z]\s*=\s*([a-zA-Z0-9$]+)(\[\d+\])\([a-z]\)',
-    ]
-    #logger.debug('Finding throttling function name')
-    for pattern in function_patterns:
-        regex = re.compile(pattern)
-        function_match = regex.search(js)
-        if function_match:
-            #logger.debug("finished regex search, matched: %s", pattern)
-            if len(function_match.groups()) == 1:
-                return function_match.group(1)
-            idx = function_match.group(2)
-            if idx:
-                idx = idx.strip("[]")
-                array = re.search(
-                    r'var {nfunc}\s*=\s*(\[.+?\]);'.format(
-                        nfunc=re.escape(function_match.group(1))),
-                    js
-                )
-                if array:
-                    array = array.group(1).strip("[]").split(",")
-                    array = [x.strip() for x in array]
-                    return array[int(idx)]
-
-    raise RegexMatchError(
-        caller="get_throttling_function_name", pattern="multiple"
-    )
-
-cipher.get_throttling_function_name = get_throttling_function_name
 
 def processlink(url, dir):
     print("Link: ", url, " Dir:", dir)
-    if "youtube.com/watch" in url or "youtu.be/" or "music.youtube.com/watch" in url:
+    if "youtube.com/watch" in url or "youtu.be/" in url:
         getYTsong(YouTube(url), dir)
-    elif "youtube.com/playlist" or "music.youtube.com/playlist" in url:
+    elif "youtube.com/playlist" in url:
         playlist = Playlist(url)
         for video in playlist.videos:
             getYTsong(video, dir)
